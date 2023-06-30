e66eabcf1475912a4efba5669c6faea2a6beb978
yuvadany <toyuvaraj@gmail.com>
2023-06-30 09:42:09 +0200

    Random quote Html page with Date

--- src/main/java/com/today/quote/util/QuoteUtil.java
+++ src/main/java/com/today/quote/util/QuoteUtil.java
@@ -6,7 +6,7 @@ public class QuoteUtil {
     public String getRandomNumber() {
         Random random = new Random();
         int min = 1;
-        int max = 100;
+        int max = 99;
         return  String.valueOf(random.nextInt(max + min) + min);
     }
 
